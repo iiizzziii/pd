ccc6bb9013d40d93c6b8d980c408a4f9f825ebd2
iiizzziii <mokos.jakub@gmail.com>
2022-11-16 22:44:10 +0100

    col max len value

--- pd.py
+++ pd.py
@@ -3,3 +3,6 @@ df[df.isna().any(axis=1)]
 
 # merge columns with some aggregator (---char---)
 df["new_col"] = df[["col_a","col_b"]].astype(str).agg("---char---".join,axis=1)
+
+# max length of value in column
+df["col"].str.len().max()
