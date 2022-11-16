945866e28b953c1541fa8e84c1870ca6ba50e9c3
iiizzziii <mokos.jakub@gmail.com>
2022-11-16 23:04:01 +0100

    Update pd.py
    col difference

--- pd.py
+++ pd.py
@@ -6,3 +6,6 @@ df["new_col"] = df[["col_a","col_b"]].astype(str).agg("---char---".join,axis=1)
 
 # max length of value in column
 df["col"].str.len().max()
+
+# display new columns in df_a or [] (ideal for comparing updated dfs)
+df_a.columns.difference(df_b.columns)
