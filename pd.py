fa19b4f60f064486576166b9243c9139f7c86acf
iiizzziii <mokos.jakub@gmail.com>
2022-11-16 21:38:48 +0100

    merge columns

--- pd.py
+++ pd.py
@@ -1,3 +1,6 @@
 
 # display df(all rows) with empty values
 df[df.isna().any(axis=1)]
+
+# merge columns with some aagregator (---char---)
+df["new_col"] = df[["col_a","col_b"]].astype(str).agg("---char---".join,axis=1)
