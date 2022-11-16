eaf7c9a172fdbba554698359c741eafe63859c6c
iiizzziii <mokos.jakub@gmail.com>
2022-11-16 21:45:30 +0100

    typo

--- pd.py
+++ pd.py
@@ -2,5 +2,5 @@
 # display df(all rows) with empty values
 df[df.isna().any(axis=1)]
 
-# merge columns with some aagregator (---char---)
+# merge columns with some aggregator (---char---)
 df["new_col"] = df[["col_a","col_b"]].astype(str).agg("---char---".join,axis=1)
